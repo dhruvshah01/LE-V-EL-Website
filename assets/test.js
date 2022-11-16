72bbfa08b5aa54cde47af40e0cf0b783a3e24e5d
Dhruv Shah <dhruvsh0111@gmail.com>
2022-11-16 15:02:06 -0500

    Added Files

---
+++ assets/test.js
@@ -0,0 +1,28 @@
+var da = `[ 
+    { "id": "1",  
+      "name": "DenseNet",  
+      "date": "Angle",
+		"score": "2.8"}, 
+	  
+    { "id": "2", 
+      "name": "VGG",  
+      "date": "Angle",
+      "score": "3.854"	  },
+{ "id": "3",  
+      "name": "AlexNet",  
+      "date": "Angle",
+		"score": "7.869"},
+{ "id": "1",  
+      "name": "AlexNet",  
+      "date": "Volume",
+		"score": "1.832"}, 
+	  
+    { "id": "2", 
+      "name": "VGG",  
+      "date": "Volume",
+      "score": "4.574"	  },
+{ "id": "3",  
+      "name": "DenseNet",  
+      "date": "Volume",
+		"score": "5.699"}		
+]`;
